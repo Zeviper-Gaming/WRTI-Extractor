139d674cc3e893af3b69d7914281aee02b4a3be8
Florian Delrieu <florian.delrieu@free.fr>
2023-06-11 12:16:11 +0200

    v0.2.1 - GPT search mex_speed

--- main.py
+++ main.py
@@ -4,19 +4,29 @@ from bs4 import BeautifulSoup
 html_content = extract_url("https://wiki.warthunder.com/V.G.33C-1")
 
 soup = BeautifulSoup(html_content, 'html.parser')
+
+# Recherche du tableau contenant les performances
 table = soup.find("table", class_="wikitable")
-
-# Recherche de la ligne correspondant au mode "RB"
-max_speed = None  # Valeur par défaut si aucune ligne n'est trouvée
-rows = table.find_all("tr")
-for row in rows:
-    columns = row.find_all("td")
-    if len(columns) > 0 and columns[0].text.strip() == "RB":
-        max_speed = columns[1].text.strip()  # Récupération de la vitesse max
-        break
-
-if max_speed is not None:
-    print("Vitesse maximale en RB :", max_speed)
+if table is None:
+    print("Tableau des performances non trouvé.")
 else:
-    print("Aucune donnée de vitesse maximale en RB trouvée.")
+    rows = table.find_all("tr")
 
+    # Recherche de l'en-tête de la colonne "RB"
+    rb_header = None
+    header_row = rows[0]
+    header_columns = header_row.find_all("th")
+    for column_index, column in enumerate(header_columns):
+        if column.text.strip() == "RB":
+            rb_header = column_index
+            break
+
+    if rb_header is not None:
+        # Recherche de la ligne correspondant à la vitesse maximale
+        speed_row = rows[1]
+        speed_columns = speed_row.find_all("td")
+        max_speed = speed_columns[rb_header].text.strip()
+
+        print("Vitesse maximale en RB :", max_speed)
+    else:
+        print("En-tête RB non trouvé.")
