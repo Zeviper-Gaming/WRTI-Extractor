e1ddc6f73c5f2ba5e7b6815fc16a6e972ca5636c
Florian Delrieu <florian.delrieu@free.fr>
2023-06-11 12:28:36 +0200

    v0.2.3 - find all wikitable

--- main.py
+++ main.py
@@ -5,28 +5,30 @@ html_content = extract_url("https://wiki.warthunder.com/V.G.33C-1")
 
 soup = BeautifulSoup(html_content, 'html.parser')
 
-# Recherche du tableau contenant les performances
-table = soup.find("table", class_="wikitable")
-if table is None:
-    print("Tableau des performances non trouvé.")
-else:
-    rows = table.find_all("tr")
+# Recherche de la table contenant les performances
+tables = soup.find_all("table", class_="wikitable")
 
-    # Recherche de l'en-tête de la colonne "RB"
-    rb_header = None
-    header_row = rows[0]
+target_table = None
+for table in tables:
+    # Vérifie si la table contient les valeurs recherchées (par exemple, en vérifiant l'en-tête)
+    header_row = table.find("tr")
     header_columns = header_row.find_all("th")
-    for column_index, column in enumerate(header_columns):
-        if column.text.strip() == "RB":
-            rb_header = column_index
+    if len(header_columns) > 1 and header_columns[1].text.strip() == "Max Speed\n(km/h at 4,500 m)":
+        target_table = table
+        break
+
+if target_table is None:
+    print("Table des performances non trouvée.")
+else:
+    # Recherche de la ligne correspondant au mode "RB"
+    rows = target_table.find_all("tr")
+    for row in rows:
+        columns = row.find_all("td")
+        if len(columns) > 0 and columns[0].text.strip() == "RB":
+            max_speed = columns[1].text.strip()  # Récupération de la vitesse max
             break
 
-    if rb_header is not None:
-        # Recherche de la ligne correspondant à la vitesse maximale
-        speed_row = rows[1]
-        speed_columns = speed_row.find_all("td")
-        max_speed = speed_columns[rb_header].text.strip()
-
+    if 'max_speed' in locals():
         print("Vitesse maximale en RB :", max_speed)
     else:
-        print("En-tête RB non trouvé.")
+        print("Vitesse maximale en RB non trouvée.")
