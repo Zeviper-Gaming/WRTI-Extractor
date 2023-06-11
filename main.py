95cb2c740f7a6902cba7a336389b01fa89ee3c44
Florian Delrieu <florian.delrieu@free.fr>
2023-06-11 12:51:11 +0200

    v0.2.5 - minor
    - try to find wings limits in the others table but cant

--- main.py
+++ main.py
@@ -5,30 +5,21 @@ html_content = extract_url("https://wiki.warthunder.com/V.G.33C-1")
 
 soup = BeautifulSoup(html_content, 'html.parser')
 
-# Recherche de la table contenant les performances
-tables = soup.find_all("table", class_="wikitable")
+# Recherche de la table contenant les limites
+table = soup.find("table", class_="wikitable")
 
-target_table = None
-for table in tables:
-    # Vérifie si la table contient les valeurs recherchées (par exemple, en vérifiant l'en-tête)
-    header_row = table.find("tr")
-    header_columns = header_row.find_all("th")
-    if len(header_columns) > 1 and "Max Speed" in header_columns[1].text.strip():
-        target_table = table
-        break
-
-if target_table is None:
-    print("Table des performances non trouvée.")
+if table is None:
+    print("Table des limites non trouvée.")
 else:
-    # Recherche de la ligne correspondant au mode "RB"
-    rows = target_table.find_all("tr")
+    # Recherche de la ligne correspondant aux limites des ailes
+    rows = table.find_all("tr")
     for row in rows:
         columns = row.find_all("td")
-        if len(columns) > 0 and columns[0].text.strip() == "RB":
-            max_speed = columns[1].text.strip()  # Récupération de la vitesse max
+        if len(columns) > 0 and columns[0].text.strip() == "Wings (km/h)":
+            wing_limits = [col.text.strip() for col in columns[1:]]  # Récupération des limites des ailes
             break
 
-    if 'max_speed' in locals():
-        print("Vitesse maximale en RB :", max_speed)
+    if 'wing_limits' in locals():
+        print("Limites des ailes (km/h) :", wing_limits)
     else:
-        print("Vitesse maximale en RB non trouvée.")
+        print("Limites des ailes non trouvées.")
