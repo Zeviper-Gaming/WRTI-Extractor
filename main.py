fc2bdfe1592963db679990a927175f78f41e600a
Florian Delrieu <florian.delrieu@free.fr>
2023-06-11 12:58:00 +0200

    v0.2.6 - minor
    - find in all tables

--- main.py
+++ main.py
@@ -6,7 +6,16 @@ html_content = extract_url("https://wiki.warthunder.com/V.G.33C-1")
 soup = BeautifulSoup(html_content, 'html.parser')
 
 # Recherche de la table contenant les limites
-table = soup.find("table", class_="wikitable")
+tables = soup.find_all("table", class_="wikitable")
+
+target_table = None
+for table in tables:
+    # Vérifie si la table contient les valeurs recherchées (par exemple, en vérifiant l'en-tête)
+    header_row = table.find("tr")
+    header_columns = header_row.find_all("th")
+    if len(header_columns) > 1 and "Limits" in header_columns[1].text.strip():
+        target_table = table
+        break
 
 if table is None:
     print("Table des limites non trouvée.")
@@ -14,8 +23,8 @@ else:
     # Recherche de la ligne correspondant aux limites des ailes
     rows = table.find_all("tr")
     for row in rows:
-        columns = row.find_all("td")
-        if len(columns) > 0 and columns[0].text.strip() == "Wings (km/h)":
+        columns = row.find_all("th")
+        if len(columns) > 0 and "Wings" in columns[0].text.strip():
             wing_limits = [col.text.strip() for col in columns[1:]]  # Récupération des limites des ailes
             break
 
