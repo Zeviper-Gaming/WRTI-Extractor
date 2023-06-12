74cb0fe90da267ecbe7d120ac45ed2b80824e5d6
Florian Delrieu <florian.delrieu@free.fr>
2023-06-12 13:44:53 +0200

    v0.3.1 - extracts datas

--- main.py
+++ main.py
@@ -14,3 +14,9 @@ tables = find_all_tables(html_content)
 limits = find_limits(tables)
 
 print(limits)
+
+wings_limit = limits[0]
+gear_limit = limits[1]
+flaps_1_limit = limits[2]
+flaps_2_limit = limits[3]
+flaps_3_limit = limits[4]
