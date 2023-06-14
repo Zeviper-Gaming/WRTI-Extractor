52293a57ba8fbbb0617933e4e8b0efaafe23ccc3
floriandelrieu <florian.delrieu@free.fr>
2023-06-14 20:50:05 +0200

    [v0.7.6] - update function.py - rename variables - modify csv format file (, and ;)

--- function.py
+++ function.py
@@ -2,11 +2,15 @@ import os
 
 def update_wtrti_data():
    os.chdir("D:\OneDrive\Logiciels et Jeux\War Thunder\HUDs\FM")
-   target_file = open("fm_data_db.csv","r")
+   source_file = open("fm_data_db.csv","r")
    os.chdir("F:\Github Local\WRTI-Extractor\config_files")
-   temp_file = open("fm_data_db_csv.temp","w")
+   new_data_file = open("fm_data_db_csv.temp","w")
 
-   for line in target_file.readlines(): temp_file.write(line)
+   for line in source_file:
+      line = line.replace(",",":")
+      line = line.replace(";",",")
+      new_data_file.write(line)
 
+   source_file.close()
 ##################### TEST ZONE
 update_wtrti_data()
