f635998913506ed62e296215de4fae6b0bafd404
floriandelrieu <florian.delrieu@free.fr>
2023-06-14 20:58:17 +0200

    [v0.7.7] - update function finnish

--- function.py
+++ function.py
@@ -4,7 +4,7 @@ def update_wtrti_data():
    os.chdir("D:\OneDrive\Logiciels et Jeux\War Thunder\HUDs\FM")
    source_file = open("fm_data_db.csv","r")
    os.chdir("F:\Github Local\WRTI-Extractor\config_files")
-   new_data_file = open("fm_data_db_csv.temp","w")
+   new_data_file = open("fm_data_db.csv","w")
 
    for line in source_file:
       line = line.replace(",",":")
