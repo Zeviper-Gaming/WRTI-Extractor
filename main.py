a65f9a1e277e7359b469c34784f1bcc261630575
floriandelrieu <florian.delrieu@free.fr>
2023-06-14 18:21:03 +0200

    [v0.7.4] - update data file - pre-treatment of data file

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import os
 import MyPack2.Saves.CSV as csv
 
 # load wtrti data files
-os.chdir("D:\OneDrive\Logiciels et Jeux\War Thunder\HUDs\FM") # go to WTRTI profile folder
+os.chdir("config_files") # go to WTRTI profile folder
 dico_fm_data_db = csv.Csv2Dict("fm_data_db.csv") #fixme can not load file as dict (see format of csv file (, and ;) )
