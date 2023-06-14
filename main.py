9eeea366eab8ec6bfb2ecade797119125b0ed5b3
floriandelrieu <florian.delrieu@free.fr>
2023-06-14 18:15:29 +0200

    [v0.7.3] - add data file - add fixme label - add fm_data_db.csv in config_files folder

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import MyPack2.Saves.CSV as csv
 
 # load wtrti data files
 os.chdir("D:\OneDrive\Logiciels et Jeux\War Thunder\HUDs\FM") # go to WTRTI profile folder
-dico_fm_data_db = csv.Csv2Dict("fm_data_db.csv")
+dico_fm_data_db = csv.Csv2Dict("fm_data_db.csv") #fixme can not load file as dict (see format of csv file (, and ;) )
