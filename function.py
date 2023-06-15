7b8374d3451335274a7256d38cf6bf068c5a5ce4
floriandelrieu <florian.delrieu@free.fr>
2023-06-15 04:46:59 +0200

    [v0.7.11] - sourcery

--- function.py
+++ function.py
@@ -3,16 +3,14 @@ import shutil
 
 def update_wtrti_data():
    os.chdir("D:\OneDrive\Logiciels et Jeux\War Thunder\HUDs\FM")
-   source_file = open("fm_data_db.csv","r")
-   os.chdir("F:\Github Local\WRTI-Extractor\config_files")
-   new_data_file = open("fm_data_db.csv","w")
+   with open("fm_data_db.csv","r") as source_file:
+      os.chdir("F:\Github Local\WRTI-Extractor\config_files")
+      new_data_file = open("fm_data_db.csv","w")
 
-   for line in source_file:
-      line = line.replace(",",":")
-      line = line.replace(";",",")
-      new_data_file.write(line)
-
-   source_file.close()
+      for line in source_file:
+         line = line.replace(",",":")
+         line = line.replace(";",",")
+         new_data_file.write(line)
 
 def generate_cfg_files(data_dico):
    path_source = "F:\Github Local\WRTI-Extractor\config_files"
