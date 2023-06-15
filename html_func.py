945bca4b55378403b48a91e74df664b73a1a7bbf
Florian Delrieu <florian.delrieu@free.fr>
2023-06-15 20:05:28 +0200

    [v1.1.2] - add some function for html

--- html_func.py
+++ html_func.py
@@ -10,7 +10,10 @@ def import_from_url(name):
    """
    dico_html_variables = {}
    url = get_url_from_name(name) # fonction imcomplète
-   url = "https://wiki.warthunder.com/D.520" # for test
+   # Extrait le code html du site
+   html_content = extract_from_url(url)
+   # Isole le code des tableau de la page
+   all_table = find_all_tables(html_content)
    return dico_html_variables
 
 def get_url_from_name(name):
@@ -23,3 +26,41 @@ def get_url_from_name(name):
    #todo Trouver comment transformer :name: en suffixe url
    url = f"https://wiki.warthunder.com/{name}"
    return url
+
+def extract_from_url(url):
+   response = requests.get(url)
+   if response.status_code == 200:
+      html_content = response.text
+      # Maintenant, tu peux traiter le contenu HTML pour extraire les valeurs qui t'intéressent
+   else:
+      print("Une erreur s'est produite lors de la récupération du contenu HTML.")
+   return html_content
+
+def find_all_tables(html_content):
+   soup = bs(html_content, 'html.parser')
+   return soup.find_all("table", class_="wikitable")
+
+
+def find_table(tables,title):
+   """
+   Trouve une table spécifique en cherchant le titre du tableau
+   :param tables:
+   :param title:
+   :return:
+   """
+   for table in tables:
+      # Vérifie si la table contient les valeurs recherchées (par exemple, en vérifiant l'en-tête)
+      header_row = table.find("tr")
+      header_columns = header_row.find_all("th")
+      if title in header_columns[0].text.strip():
+         return table
+      else:
+         print("Table specifié non trouvée.")
+
+def extract_last_line_from_table(table):
+   # Recherche de la dernière ligne du tableau "Limits"
+   rows = table.find_all("tr")
+   last_row = rows[-1]
+   # Extraction des valeurs de la dernière ligne
+   values = [col.text.strip() for col in last_row.find_all("td")]
+   return values
