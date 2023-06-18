b1276ecbdcfed022eafac25a4fcf6d3bf148a474
floriandelrieu <florian.delrieu@free.fr>
2023-06-18 02:42:29 +0200

    [URL v1.6.1b] - add p63 france in url list

--- url_list.py
+++ url_list.py
@@ -1,3 +1,4 @@
 def get_suffix_from_list(name):
     if name == "vg-33"                  :   return "V.G.33C-1"
     if name == "d520_france"            :   return "D.520"
+    if name == "p-63c-5_(france)"       :   return "P-63C-5_(France)"
