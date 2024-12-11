9cb70fdf0d1b6eff921352e4ec317e39464419c0
floriandelrieu <florian.delrieu@free.fr>
2024-12-11 05:43:00 +0100

    [2.5.16] - add found_in_json.py (TEST)
    - Pour checher des mots clès dans les json

---
+++ TESTING_HALL/found_in_json.py
@@ -0,0 +1,39 @@
+import os
+
+def search_for_keyword_in_files(keyword, directory="."):
+    """
+    Recherche les fichiers contenant un mot-clé spécifique dans un dossier donné.
+
+    Args:
+        keyword (str): Le mot-clé à rechercher.
+        directory (str): Le chemin du dossier où effectuer la recherche.
+
+    Returns:
+        list: Liste des fichiers contenant le mot-clé.
+    """
+    matching_files = []
+
+    for filename in os.listdir(directory):
+        if filename.endswith(".json"):
+            file_path = os.path.join(directory, filename)
+            try:
+                with open(file_path, 'r') as file:
+                    content = file.read()
+                    if keyword in content:
+                        matching_files.append(filename)
+            except Exception as e:
+                print(f"Erreur lors de la lecture du fichier {filename}: {e}")
+
+    return matching_files
+
+if __name__ == "__main__":
+    keyword = "MinimalSpeed"
+    directory = "F:\Github Local\WRTI-Extractor\datas\json_files"
+    files = search_for_keyword_in_files(keyword, directory)
+
+    if files:
+        print("Fichiers contenant le mot-clé '{}':".format(keyword))
+        for file in files:
+            print(f"- {file}")
+    else:
+        print("Aucun fichier ne contient le mot-clé '{}'.".format(keyword))
